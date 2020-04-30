8896fcf408eb5354fe6fa58ffc0504a7f74f907e
William Mittard <billymittard@ymail.com>
2020-04-30 00:27:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,10 +2,10 @@ class Triangle
   attr_accessor :length_1,:length_2,:length_3  
   
   def initialize(length_1,length_2,length_3)
-    triangle_length = []
-    @length_1 << length_1
-    @length_2 << length_2 
-    @length_3 << length_3
+    @triangle_length = []
+    @triangle_length << length_1
+    @triangle_length << length_2 
+    @triangle_length << length_3
    
   end 
   class TriangleError < StandardError
