4655b8cd06f1e101a709c675227b69e6e6051141
William Mittard <billymittard@ymail.com>
2020-04-30 00:21:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,4 +1,10 @@
 class Triangle 
   attr_accessor :equilateral, :isosceles, :scalene 
   
+  def initialize(equilateral,isosceles,scalene)
+    @equilateral = equilateral 
+    @isosceles = isosceles 
+    @scalene = scalene 
+  end 
+  
 end
